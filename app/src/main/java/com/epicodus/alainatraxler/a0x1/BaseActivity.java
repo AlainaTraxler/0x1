20df47d7e87d464ed88ce49539cbaa2059013bd6
Alaina Traxler <alainaltraxler@gmail.com>
2016-12-19 13:23:05 -0800

    refactor Base to use AuthStateListener for redirects.

--- app/src/main/java/com/epicodus/alainatraxler/a0x1/BaseActivity.java
+++ app/src/main/java/com/epicodus/alainatraxler/a0x1/BaseActivity.java
@@ -2,32 +2,58 @@ package com.epicodus.alainatraxler.a0x1;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class BaseActivity extends AppCompatActivity {
-    FirebaseAuth mAuth;
+    public FirebaseAuth mAuth;
+    public Context mContext;
 
-    Context mContext;
+    public String TAG;
+    public FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mAuth = FirebaseAuth.getInstance();
-
         mContext = this;
 
-        checkForRedirect();
+        TAG = this.getClass().getSimpleName();
+
+        // Checks to see if user is logged in, and if not, redirects them to Login
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                } else if(!(mContext instanceof LoginActivity)){
+                    Intent intent = new Intent(mContext, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
     }
 
-    // Checks to see if user is logged in, and if not, redirects them to Login
-    public void checkForRedirect(){
-        if(mAuth.getCurrentUser() == null && !this.getClass().getSimpleName().equals("LoginActivity")){
-            Intent intent = new Intent(mContext, LoginActivity.class);
-            startActivity(intent);
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    // [END on_start_add_listener]
+
+    // [START on_stop_remove_listener]
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 }

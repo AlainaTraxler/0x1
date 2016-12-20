a6b8f8c1c13a5f3bf47df1f00c6af4204f6886f1
Alaina Traxler <alainaltraxler@gmail.com>
2016-12-19 16:59:07 -0800

    add twin RecyclerViews on Build.

--- app/src/main/java/com/epicodus/alainatraxler/a0x1/ui/BuildActivity.java
+++ app/src/main/java/com/epicodus/alainatraxler/a0x1/ui/BuildActivity.java
@@ -2,14 +2,92 @@ package com.epicodus.alainatraxler.a0x1.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import com.epicodus.alainatraxler.a0x1.Constants;
 import com.epicodus.alainatraxler.a0x1.R;
+import com.epicodus.alainatraxler.a0x1.adapters.FromExerciseAdapter;
+import com.epicodus.alainatraxler.a0x1.models.Exercise;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
-public class BuildActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class BuildActivity extends BaseActivity {
+    @Bind(R.id.recyclerViewFrom) RecyclerView mRecyclerViewFrom;
+    @Bind(R.id.recyclerViewTo) RecyclerView mRecyclerViewTo;
+
+    private FromExerciseAdapter mFromAdapter;
+    private ArrayList<Exercise> mExercises = new ArrayList<Exercise>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_build);
+        ButterKnife.bind(this);
+
+//        DisplayMetrics displaymetrics = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+//        int height = displaymetrics.heightPixels;
+//        int width = displaymetrics.widthPixels;
+//        mRecyclerViewFrom.setLayoutParams(new RelativeLayout.LayoutParams(width/2, height));
+//
+//        RecyclerView.LayoutParams params = new RecyclerView.LayoutParams(width/2, height);
+
+
+        mFromAdapter = new FromExerciseAdapter(getApplicationContext(), mExercises);
+        mRecyclerViewFrom.setAdapter(mFromAdapter);
+        RecyclerView.LayoutManager FromLayoutManager =
+                new LinearLayoutManager(BuildActivity.this);
+        mRecyclerViewFrom.setLayoutManager(FromLayoutManager);
+        mRecyclerViewFrom.setHasFixedSize(true);
+
+        mRecyclerViewTo.setAdapter(mFromAdapter);
+        RecyclerView.LayoutManager ToLayoutManager =
+                new LinearLayoutManager(BuildActivity.this);
+        mRecyclerViewTo.setLayoutManager(ToLayoutManager);
+        mRecyclerViewTo.setHasFixedSize(true);
+
+        getExercises();
+    }
+
+    public void getExercises(){
+        dbExercises.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                mExercises.add(dataSnapshot.getValue(Exercise.class));
+                Log.v(TAG, dataSnapshot.getValue(Exercise.class).getName());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }

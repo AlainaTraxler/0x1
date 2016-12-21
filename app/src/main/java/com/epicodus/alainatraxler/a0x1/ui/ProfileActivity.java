9e27d4f14a16ae31ea7b33d875a659b12f95e668
Alaina Traxler <alainaltraxler@gmail.com>
2016-12-20 18:18:33 -0800

    add stat display for Profile.

--- app/src/main/java/com/epicodus/alainatraxler/a0x1/ui/ProfileActivity.java
+++ app/src/main/java/com/epicodus/alainatraxler/a0x1/ui/ProfileActivity.java
@@ -2,14 +2,87 @@ package com.epicodus.alainatraxler.a0x1.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.epicodus.alainatraxler.a0x1.Constants;
 import com.epicodus.alainatraxler.a0x1.R;
+import com.epicodus.alainatraxler.a0x1.models.Exercise;
+import com.epicodus.alainatraxler.a0x1.models.Workout;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
-public class ProfileActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ProfileActivity extends BaseActivity {
+    @Bind(R.id.Workouts) TextView mWorkouts;
+    @Bind(R.id.Moved) TextView mMoved;
+    @Bind(R.id.Weight) TextView mWeight;
+
+    long mTotalWeightMoved = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+
+        setWorkouts();
+        setMoved();
+    }
+
+    private void setWorkouts(){
+        dbCurrentUser.child(Constants.DB_NODE_WORKOUTS).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mWorkouts.setText(String.valueOf(dataSnapshot.getChildrenCount()));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setMoved(){
+        dbCurrentUser.child(Constants.DB_NODE_WORKOUTS).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Workout workout = dataSnapshot.getValue(Workout.class);
+                ArrayList<Exercise> exercises = workout.getExercises();
+
+                for(int i = 0; i < exercises.size(); i++){
+                    Exercise exercise = exercises.get(i);
+                    mTotalWeightMoved += exercise.getSets() * exercise.getReps() * exercise.getWeight();
+                }
+
+                mMoved.setText(String.valueOf(mTotalWeightMoved));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
